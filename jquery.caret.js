bdc39cc47d7e9a31825dd23f84958368095c0c01
Gideon Sireling <code@accursoft.com>
2012-04-23 14:32:41 +0300

    comments

--- jquery.caret.js
+++ jquery.caret.js
@@ -1,27 +1,31 @@
-﻿(function($) {
+//get the caret's position: pos = $(textarea).caret()
+//set the caret's position: $(textarea).caret(pos)
+(function($) {
   $.fn.caret = function(pos) {
     var target = this[0];
-    if (arguments.length == 0) { //get
-      if (target.selectionStart) { //DOM
+    //get
+    if (arguments.length == 0) {
+      if (target.selectionStart) { //HTML5
         var pos = target.selectionStart;
         return pos > 0 ? pos : 0;
       }
-      else if (target.createTextRange) { //IE
-		target.focus();
-		var range = document.selection.createRange();
-		if (range == null)
-			return '0';
-		var re = target.createTextRange();
-		var rc = re.duplicate();
-		re.moveToBookmark(range.getBookmark());
-		rc.setEndPoint('EndToStart', re);
-		return rc.text.length;
+      else if (target.createTextRange) { //IE<9
+        target.focus();
+        var range = document.selection.createRange();
+        if (range == null)
+            return '0';
+        var re = target.createTextRange();
+        var rc = re.duplicate();
+        re.moveToBookmark(range.getBookmark());
+        rc.setEndPoint('EndToStart', re);
+        return rc.text.length;
       }
-      else return 0;
-    } //set
-    if (target.setSelectionRange) //DOM
+      else return 0; //not supported
+    }
+    //set
+    if (target.setSelectionRange) //HTML5
       target.setSelectionRange(pos, pos);
-    else if (target.createTextRange) { //IE
+    else if (target.createTextRange) { //IE<9
       var range = target.createTextRange();
       range.collapse(true);
       range.moveEnd('character', pos);
